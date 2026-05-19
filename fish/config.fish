abe19df72a3e7c08232522372161f7f57e10499a
LimeUther <uther4ball@gmail.com>
2026-05-19 09:36:26 +0800

    I added fish :D

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+  set fish_greeting
+
+  alias clear "printf '\033[2J\033[3J\033[1;1H'"
+  alias celar "printf '\033[2J\033[3J\033[1;1H'"
+  alias claer "printf '\033[2J\033[3J\033[1;1H'"
+end
